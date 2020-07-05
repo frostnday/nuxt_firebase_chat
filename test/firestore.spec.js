cadd735527dc73d27e5d68026b623b462a8be76b
frostnday <frostnday@gmail.com>
2020-07-06 03:31:08 +0900

    4-4 Firestore のセキュリティルールの設定

---
+++ test/firestore.spec.js
@@ -0,0 +1,115 @@
+import * as fs from 'fs'
+import * as firebase from '@firebase/testing'
+
+const testName = 'firestore-test'
+
+describe(testName, () => {
+  beforeAll(async () => {
+    await firebase.loadFirestoreRules({
+      projectId: testName,
+      rules: fs.readFileSync('firestore.rules', 'utf8')
+    })
+  })
+
+  // テスト終了後にデータを削除します
+  afterEach(async () => {
+    await firebase.clearFirestoreData({ projectId: testName })
+  })
+
+  // テスト終了後にアプリを削除します
+  afterAll(async () => {
+    await Promise.all(firebase.apps().map(app => app.delete()))
+  })
+
+  // 認証済のFirestore
+  function authDB(auth) {
+    return firebase
+      .initializeTestApp({ projectId: testName, auth: auth })
+      .firestore()
+  }
+
+  // 未認証のFirestore
+  function noAuthDB() {
+    return firebase
+      .initializeTestApp({ projectId: testName, auth: null })
+      .firestore()
+  }
+
+  describe('users collection', () => {
+    describe('read', () => {
+      test('未ログインの場合、ユーザーデータ取得に失敗するかどうか', async () => {
+        const db = noAuthDB()
+        await firebase.assertFails(
+          db
+            .collection('users')
+            .doc('tech-user')
+            .get()
+        )
+      })
+
+      test('ログイン済の場合、ユーザーデータを取得できるかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertSucceeds(
+          db
+            .collection('users')
+            .doc('tech-user')
+            .get()
+        )
+      })
+    })
+
+    describe('create', () => {
+      test('自分のアカウントのデータを作成できるかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertSucceeds(
+          db
+            .collection('users')
+            .doc('tech-user')
+            .set({
+              name: 'testUser',
+              iconImageUrl: 'https://exampla.com'
+            })
+        )
+      })
+
+      test('ユーザー名が未入力の場合、作成に失敗するかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertFails(
+          db
+            .collection('users')
+            .doc('tech-user')
+            .set({
+              name: '',
+              iconImageUrl: 'https://exampla.com'
+            })
+        )
+      })
+
+      test('アイコン画像が未入力の場合、作成に失敗するかどうか', async () => {
+        const db = authDB({ uid: 'tech-user' })
+        await firebase.assertFails(
+          db
+            .collection('users')
+            .doc('tech-user')
+            .set({
+              name: 'testUser',
+              iconImageUrl: ''
+            })
+        )
+      })
+
+      test('他人のアカウントのデータは作成に失敗するかどうか', () => {
+        const db = authDB({ uid: 'tech-user' })
+        firebase.assertFails(
+          db
+            .collection('users')
+            .doc('tech-user2')
+            .set({
+              name: 'testUser',
+              iconImageUrl: 'https://exampla.com'
+            })
+        )
+      })
+    })
+  })
+})
