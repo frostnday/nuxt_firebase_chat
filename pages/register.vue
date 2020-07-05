26816fda0b0be5c4357e60634fc3bd06ea3a8f93
frostnday <frostnday@gmail.com>
2020-07-06 02:56:38 +0900

    4-2 画像 upload 機能の作成

--- pages/register.vue
+++ pages/register.vue
@@ -6,14 +6,24 @@
         <div
           class="flex items-center justify-center w-32 h-32 bg-gray-200 rounded-full border border-gray-400 "
         >
-          <i class="material-icons text-7xl text-gray">
-            person
-          </i>
+          <template v-if="form.imageUrl.val">
+            <img
+              :src="form.imageUrl.val"
+              class="w-32 h-32 object-cover border rounded-full"
+              @click="selectImage"
+            />
+          </template>
+          <template v-else>
+            <i class="material-icons text-7xl text-gray" @click="selectImage">
+              person
+            </i>
+          </template>
           <input
             ref="image"
             type="file"
             style="display: none"
             accept="image/*"
+            @change="onSelectFile"
           />
         </div>
       </div>
@@ -40,3 +50,55 @@
     </form>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        imageUrl: {
+          val: null
+        }
+      }
+    }
+  },
+
+  methods: {
+    selectImage() {
+      this.$refs.image.click()
+    },
+
+    onSelectFile(e) {
+      const files = e.target.files
+      if (files.length === 0) return
+
+      const reader = new FileReader()
+      reader.readAsDataURL(files[0])
+
+      reader.addEventListener('load', () => {
+        this.upload({
+          localImageFile: files[0]
+        })
+      })
+    },
+
+    async upload({ localImageFile }) {
+      const user = await this.$auth()
+
+      // 未ログインの場合
+      if (!user) this.$router.push('/login')
+
+      // // ストレージオブジェクト作成
+      const storageRef = this.$fireStorage.ref()
+
+      // ファイルのパスを設定
+      const imageRef = storageRef.child(
+        `images/${user.uid}/${localImageFile.name}`
+      )
+
+      // ファイルを適用してファイルアップロード開始
+      const snapShot = await imageRef.put(localImageFile)
+      this.form.imageUrl.val = await snapShot.ref.getDownloadURL()
+    }
+  }
+}
+</script>
