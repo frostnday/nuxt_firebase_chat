dc527569cd2bd92e7379109850d95ab65f32df55
frostnday <frostnday@gmail.com>
2020-07-08 00:57:26 +0900

    6-2 チャット投稿機能の作成

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,24 @@
 <template>
   <header class="fixed top-0 bg-lightblue w-full max-w-sm h-16 py-4 z-10">
     <div class="relative flex justify-center w-full">
+      <template v-if="isChatPage">
+        <nuxt-link
+          to="/"
+          class="absolute left-20 flex items-center justify-center no-underline"
+        >
+          <i class="material-icons text-4xl text-white">chevron_left</i>
+        </nuxt-link>
+      </template>
       <h1 class="text-2xl font-semibold text-white">tech-chat</h1>
     </div>
   </header>
 </template>
+<script>
+export default {
+  computed: {
+    isChatPage() {
+      return this.$route.path.match(/\/rooms\/[A-Za-z0-9]*/)
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
       <div
         v-for="room in rooms"
         :key="room.id"
+        @click="moveToRoomPage(room.id)"
         class="bg-white max-w-sm rounded-lg overflow-hidden shadow m-4 mb-5 p-4 h-32"
       >
         <div>
@@ -66,6 +67,9 @@ export default {
   },
 
   methods: {
+    moveToRoomPage(roomId) {
+      this.$router.push(`/rooms/${roomId}`)
+    },
     openModal() {
       this.isCreateMode = true
     },
