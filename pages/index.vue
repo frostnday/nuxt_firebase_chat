715270fa5520215d0a0fdbc520a14fa2ce6a47b3
frostnday <frostnday@gmail.com>
2020-07-08 00:50:31 +0900

    5-3 チャットルーム一覧表示機能の作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <div>
+      <div
+        v-for="room in rooms"
+        :key="room.id"
+        class="bg-white max-w-sm rounded-lg overflow-hidden shadow m-4 mb-5 p-4 h-32"
+      >
+        <div>
+          <img
+            :src="room.topImageUrl"
+            class="float-left object-cover rounded-lg w-24 h-24 mr-4"
+          />
+          <p class="font-mono text-darkGray">{{ room.name }}</p>
+        </div>
+      </div>
+    </div>
     <!-- ルーム作成ボタン -->
     <div class="fixed flex justify-end bottom-0 w-full max-w-sm">
       <button @click="openModal">
@@ -16,6 +31,7 @@
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 import ModalBase from '~/components/ModalBase'
 import CreateRoomModal from '~/components/CreateRoomModal'
 
@@ -28,8 +44,25 @@ export default {
 
   data() {
     return {
-      isCreateMode: false
+      isCreateMode: false,
+      unsubscribe: null
     }
+  },
+
+  computed: {
+    ...mapGetters('rooms', ['rooms'])
+  },
+
+  async asyncData({ store }) {
+    const unsubscribe = await store.dispatch('rooms/subscribe')
+    return {
+      unsubscribe
+    }
+  },
+
+  destroyed() {
+    this.$store.dispatch('rooms/clear')
+    if (this.unsubscribe) this.unsubscribe()
   },
 
   methods: {
